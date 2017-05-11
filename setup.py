62afdc15aa958d126e171c5870a65748affd98b4
selurvedu <selurvedu@users.noreply.github.com>
2017-05-11 16:23:20 +0000

    Specify broker name

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-broker_name = 'template'
+broker_name = 'tirex'
 pkg_name = 'robot_tests.broker.{}'.format(broker_name)
 description = '{} broker for OpenProcurement Robot tests'.format(broker_name)
 
